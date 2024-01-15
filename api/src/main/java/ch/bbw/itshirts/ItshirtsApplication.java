9adff06ac57cea68e16623f3a0cde65b8fcf9211
André De Coi <dre@MacBook-Air-von-Andre.local>
2024-01-15 07:57:02 +0100

    Add Customer Repository and Application Class

--- api/src/main/java/ch/bbw/itshirts/ItshirtsApplication.java
+++ api/src/main/java/ch/bbw/itshirts/ItshirtsApplication.java
@@ -2,8 +2,10 @@ package ch.bbw.itshirts;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ItshirtsApplication {
@@ -11,7 +13,42 @@ public class ItshirtsApplication {
 	private static final Logger log = LoggerFactory.getLogger(ItshirtsApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(ItshirtsApplication.class, args);
+		SpringApplication.run(ItshirtsApplication.class);
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			// save a few customers
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			repository.findAll().forEach(customer -> {
+				log.info(customer.toString());
+			});
+			log.info("");
+
+			// fetch an individual customer by ID
+			Customer customer = repository.findById(1L);
+			log.info("Customer found with findById(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+			log.info("");
+
+			// fetch customers by last name
+			log.info("Customer found with findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			repository.findByLastName("Bauer").forEach(bauer -> {
+				log.info(bauer.toString());
+			});
+			log.info("");
+		};
 	}
 
 }
