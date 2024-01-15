b1b8480c205b443292a03ec857f60624d62b5793
André De Coi <dre@MacBook-Air-von-Andre.local>
2024-01-15 07:51:10 +0100

    Add Customer Entity

--- api/src/main/java/ch/bbw/itshirts/Customer.java
+++ api/src/main/java/ch/bbw/itshirts/Customer.java
@@ -7,7 +7,36 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Customer {
+
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private long id;
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer() {}
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
